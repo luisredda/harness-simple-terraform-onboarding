d6bc8e78c084446b14e79c116ca0e45603352057
Luis Redda <luis.redda@gmail.com>
2023-04-27 19:26:57 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "api_key" {
+  description = "API key for the Harness account"
+}
+
+variable "account_id" {
+  description = "ID of the Harness account"
+}
+
+variable "project_identifier" {
+  description = "Identifier for the Harness project"
+}
+
+variable "project_name" {
+  description = "Name for the Harness project"
+}
+
+variable "org_id" {
+  description = "ID of the Harness organization"
+}
+
+variable "project_color" {
+  description = "Color for the Harness project"
+}
+
+variable "service_identifier" {
+  description = "Identifier for the Harness service"
+}
+
+variable "service_name" {
+  description = "Name for the Harness service"
+}
+
+variable "service_description" {
+  description = "Description for the Harness service"
+}
