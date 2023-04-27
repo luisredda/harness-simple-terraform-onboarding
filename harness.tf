5d7527efd2b8c32baf622f4a0def355092757649
Luis Redda <luis.redda@gmail.com>
2023-04-27 19:12:29 -0300

    Create harness.tf

---
+++ harness.tf
@@ -0,0 +1,60 @@
+provider "harness" {
+  api_key    = var.api_key
+  account_id = var.account_id
+}
+
+resource "harness_platform_project" "my_project" {
+  identifier = var.project_identifier
+  name       = var.project_name
+  org_id     = var.org_id
+  color      = var.project_color
+}
+
+resource "harness_platform_service" "my_service" {
+  identifier  = var.service_identifier
+  name        = var.service_name
+  description = var.service_description
+  org_id      = var.org_id
+  project_id  = harness_platform_project.my_project.id
+    
+  yaml = <<-EOT
+    service:
+      name: ${var.service_name}
+      identifier: ${var.identifier}
+      serviceDefinition:
+        spec:
+          manifests:
+            - manifest:
+                identifier: manifest1
+                type: K8sManifest
+                spec:
+                  store:
+                    type: Github
+                    spec:
+                      connectorRef: <+input>
+                      gitFetchType: Branch
+                      paths:
+                        - files1
+                      repoName: <+input>
+                      branch: master
+                  skipResourceVersioning: false
+          configFiles:
+            - configFile:
+                identifier: configFile1
+                spec:
+                  store:
+                    type: Harness
+                    spec:
+                      files:
+                        - <+org.description>
+          variables:
+            - name: var1
+              type: String
+              value: val1
+            - name: var2
+              type: String
+              value: val2
+        type: Kubernetes
+      gitOpsEnabled: false
+  EOT
+}
