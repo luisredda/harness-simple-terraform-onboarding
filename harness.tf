4a55ef3f1bd18640f668073abd47ac02afa372a0
Luis Redda <luis.redda@gmail.com>
2023-04-27 19:50:48 -0300

    Update harness.tf

--- harness.tf
+++ harness.tf
@@ -67,3 +67,62 @@ resource "harness_platform_service" "my_service" {
       gitOpsEnabled: false
   EOT
 }
+
+resource "harness_platform_environment" "example" {
+  identifier = var.serviceid
+  name       = var.servicename
+  org_id     = var.org_id
+  project_id = harness_platform_project.my_project.id
+  tags       = ["foo:bar", "baz"]
+  type       = "PreProduction"
+
+  ## ENVIRONMENT V2 Update
+  ## The YAML is needed if you want to define the Environment Variables and Overrides for the environment
+  ## Not Mandatory for Environment Creation nor Pipeline Usage
+
+  yaml = <<-EOT
+               environment:
+         name: name
+         identifier: identifier
+         orgIdentifier: ${var.org_id}
+         projectIdentifier: ${harness_platform_project.my_project.id}
+         type: PreProduction
+         tags:
+           foo: bar
+           baz: ""
+         variables:
+           - name: envVar1
+             type: String
+             value: v1
+             description: ""
+           - name: envVar2
+             type: String
+             value: v2
+             description: ""
+         overrides:
+           manifests:
+             - manifest:
+                 identifier: manifestEnv
+                 type: Values
+                 spec:
+                   store:
+                     type: Git
+                     spec:
+                       connectorRef: <+input>
+                       gitFetchType: Branch
+                       paths:
+                         - file1
+                       repoName: <+input>
+                       branch: master
+           configFiles:
+             - configFile:
+                 identifier: configFileEnv
+                 spec:
+                   store:
+                     type: Harness
+                     spec:
+                       files:
+                         - account:/Add-ons/svcOverrideTest
+                       secretFiles: []
+      EOT
+}
